0d5352aad57ba69d6d75bae2273786a370250ea4
Unstable <unstablectrl@gmail.com>
2017-05-08 13:01:08 +0100

    modify talks for data stream

--- src/pages/talks/talks.ts
+++ src/pages/talks/talks.ts
@@ -14,6 +14,47 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class Talks {
 
+  monday = [
+        {
+            "title": "Portal do Investigador",
+            "authors": ["António Rosado", "João Fernandes"],
+            "start": "10h 05",
+            "duration": "1h",
+            "tags": ["web", "wordpress", "database"]
+        },
+        {
+            "title": "Monitorização Automática",
+            "authors": ["André Carvalho, José Rodrigues"],
+            "start": "11h 25",
+            "duration": "1h 30",
+            "tags": ["Câmara RGBD", "SIMON", "Artificial Intelligence"]
+        },
+        {
+            "title": "Indexação e Pesquisa de Documentos ",
+            "authors": ["Bruno Dias"],
+            "start": "13h 10",
+            "duration": "1h",
+            "tags": ["OCR", "OutSystems"]
+        }
+    ];
+
+    tuesday = [
+        {
+            "title": "Seguimento de Eventos",
+            "authors": ["João Barreiros"],
+            "start": "14h 30",
+            "duration": "1h",
+            "tags": ["Ionic", "Angular", "Node.js", "Django", "Mobile"]
+        },
+        {
+            "title": "Trela Invisivel",
+            "authors": ["Inês Leandro"],
+            "start": "15h 45",
+            "duration": "2h",
+            "tags": ["Artificial Intelligence", "Fisical Computation", "GPS"]
+        }
+    ]
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
